3b56b51542c7dacfd5880e8b66f9baa92cfac703
Pablo George <pablogeokar@hotmail.com>
2024-02-11 11:52:13 -0300

    criating sidebars

--- components/Live.tsx
+++ components/Live.tsx
@@ -14,7 +14,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
   const [cursorState, setCursorState] = useState<CursorState>({
@@ -153,13 +157,14 @@ const Live = () => {
   }, [updateMyPresence]);
   return (
     <div
+      id="canvas"
       onPointerMove={handlePointerMove}
       onPointerDown={handlePointerDown}
       onPointerLeave={handlePointerLeave}
       onPointerUp={handlePointerUp}
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+      <canvas ref={canvasRef} />
 
       {reaction.map((reaction) => (
         <FlyingReaction
