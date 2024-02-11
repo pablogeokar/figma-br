606f276bfd597dcefc3b5f1c471b0a4043e73f0c
Pablo George <pablogeokar@hotmail.com>
2024-02-11 11:11:31 -0300

    creating Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div className="">
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { NavbarProps } from "@/types/type";
+import Image from "next/image";
+import ActiveUsers from "./users/ActiveUsers";
+
+const Navbar = ({ activeElement }: NavbarProps) => {
+  return (
+    <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
+      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+      <ActiveUsers />
+    </nav>
+  );
+};
+
+export default Navbar;
